0711ef64fdbbe70b1683674a9d04e607662feb6d
atosborges00 <atosborges00@gmail.com>
2021-06-11 16:14:06 -0300

    Adding the project's configuration files
    Adding the configurations of the project's paths, the configurations file for the sices platform and configuration files for the auroravision platform

---
+++ config/ConfigSices.py
@@ -0,0 +1,9 @@
+from config.ConfigPaths import ConfigPaths
+
+
+class ConfigSices:
+    
+    """ Sices login page URL """
+    LOGIN_URL = 'https://monitoramento.sicessolar.com.br/login'
+
+    """ Paths for the files containing login information """

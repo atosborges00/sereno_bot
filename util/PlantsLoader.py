787eaff93bb8d6bc301ff271b90ee16f96c105aa
atosborges00 <atosborges00@gmail.com>
2021-06-14 13:21:44 -0300

    Implemented the properties from the PlantsLoader class

--- util/PlantsLoader.py
+++ util/PlantsLoader.py
@@ -5,7 +5,40 @@ from util.BaseLoader import BaseLoader
 
 class PlantsLoader(BaseLoader):
 
-    """ Loading the Plants' data as a table to the workspace """
+    """ Loading the Plants' data as a table to the workspace and setting the properties """
 
     def __init__(self, file_path) -> None:
         super().__init__(file_path)
+
+        # mapping the columns-names to a dictionary
+        self._COLUMN_MAP = {column: self._table[0].index(column) for column in self._table[0]}
+        # deleting the columns-names row from the table
+        del self._table[0]
+
+    @property
+    def id(self) -> list:
+        return self.get_column(self._COLUMN_MAP['id'])
+
+    @property
+    def platform(self) -> list:
+        return self.get_column(self._COLUMN_MAP['platform'])
+
+    @property
+    def plant_name(self) -> list:
+        return self.get_column(self._COLUMN_MAP['plant_name'])
+
+    @property
+    def installed_power(self) -> list:
+        return self.get_column(self._COLUMN_MAP['installed_power'])
+
+    @property
+    def code(self) -> list:
+        return self.get_column(self._COLUMN_MAP['code'])
+
+    @property
+    def login_info(self) -> list:
+        return self.get_column(self._COLUMN_MAP['login_info'])
+
+    @property
+    def client_id(self) -> list:
+        return self.get_column(self._COLUMN_MAP['client_id'])

a728fa0a6111266f88a18b635cc65892a75b9d45
atosborges00 <atosborges00@gmail.com>
2021-06-14 10:16:05 -0300

    Implementing the BaseLoader class to be extended by the specific loading classes

---
+++ util/BaseLoader.py
@@ -0,0 +1,19 @@
+from csv import reader
+
+""" Super class for importing the CSV files from the database """
+
+
+class BaseLoader:
+
+    """ BaseLoader super class for importing CSV files """
+    def __init__(self, file_path):
+        with open(file_path, newline='\n', encoding='utf-8') as file:
+            self.table = list(reader(file, delimiter=";"))
+
+    """ Common methods for all children classes """
+
+    def get_column(self, column_number):
+        return [element[column_number] for element in self.table]
+
+    def get_row(self, row_number):
+        return self.table[row_number]

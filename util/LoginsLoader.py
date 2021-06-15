8e8f11ac2fecd9d97f2fff2ea790e575cf95d086
atosborges00 <atosborges00@gmail.com>
2021-06-15 16:45:02 -0300

    Implementing LoginsLoader properties matching the columns on the dataset

--- util/LoginsLoader.py
+++ util/LoginsLoader.py
@@ -8,4 +8,16 @@ class LoginsLoader(BaseLoader):
     """ Loading the Logins to the application's workspace and setting its properties """
 
     def __init__(self, file_path) -> None:
-        super(LoginsLoader, self).__init__(file_path)
+        super().__init__(file_path)
+
+        self._COLUMN_MAP = {column: self._table[0].index(column) for column in self._table[0]}
+
+    """ Properties for the Logins object """
+
+    @property
+    def logins(self) -> list:
+        return self._get_column(self._COLUMN_MAP['logins'])
+
+    @property
+    def passwords(self) -> list:
+        return self._get_column(self._COLUMN_MAP['passwords'])

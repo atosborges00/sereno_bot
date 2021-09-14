895474af6f9c5d64f52ec09a3dd3bcb0ae8505dc
atosborges00 <atosborges00@gmail.com>
2021-09-14 11:14:31 -0300

    Testing the Sices functions implemented so far. To be deleted later.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from config.ConfigPaths import ConfigPaths
+from util.LoginsLoader import LoginsLoader
+from util.PlantsLoader import PlantsLoader
+from modules.SicesPlatform import SicesPlatform
 
 if __name__ == '__main__':
-    pass
+    """ Main function of the application """
+
+    plants = PlantsLoader(ConfigPaths.PLANTS)
+    keys = LoginsLoader(ConfigPaths.LOGINS)
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    sices = SicesPlatform(driver)
+    plataforma = plants.platform_names.index('SICES')
+
+    sices.do_login(keys.logins[plataforma], keys.passwords[plataforma])
+
+    sices.get_analytics_page(plants.codes[0])

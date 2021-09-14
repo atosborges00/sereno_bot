1e1c2a87c0ab0112f128f574bbdfb58db1d39d49
atosborges00 <atosborges00@gmail.com>
2021-09-14 16:07:49 -0300

    Creating the get_yesterday_analytics function to open the data from yesterdar on the platform

--- modules/SicesPlatform.py
+++ modules/SicesPlatform.py
@@ -13,6 +13,7 @@ class SicesPlatform(BasePage):
     _LOGIN_BUTTON_LOCATOR = (By.XPATH, "//button[@type['submit']]")
     _UNITS_BUTTON_LOCATOR = (By.XPATH, "//a[@id='linkUnidades']")
     _CALENDAR_BUTTON_LOCATOR = (By.XPATH, "//div[@id='calendario']")
+    _YESTERDAY_BUTTON_LOCATOR = (By.XPATH, "//li[@data-range-key='Ontem']")
 
     """ Class constructor extending BasePage """
 
@@ -46,3 +47,13 @@ class SicesPlatform(BasePage):
 
         if calendar_button_clickable:
             self.do_click(self._CALENDAR_BUTTON_LOCATOR)
+
+    def get_yesterday_analytics(self):
+        self._open_calendar()
+        yesterday_button_clickable = self.is_clickable(self._YESTERDAY_BUTTON_LOCATOR)
+
+        if not yesterday_button_clickable:
+            raise RuntimeError("Unable to interact with the 'Ontem' button")
+
+        if yesterday_button_clickable:
+            self.do_click(self._YESTERDAY_BUTTON_LOCATOR)

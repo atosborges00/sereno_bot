53c5eafcb2f19c2f31df6cc37d093e1189f45b78
atosborges00 <atosborges00@gmail.com>
2021-10-01 14:52:22 -0300

    Defining the initializing function of the SicesController class

--- controllers/SicesController.py
+++ controllers/SicesController.py
@@ -3,6 +3,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from modules.SicesPlatform import SicesPlatform
 
 
-def run():
-    """ Function responsible for controlling the Sices Page during the data download """
-    pass
+class SicesController:
+
+    def __init__(self, plants, keys):
+
+        self.LOGINS = keys.logins[plants.sices['starting_index']:plants.sices['ending_index']]
+        self.PASSWORDS = keys.passwords[plants.sices['starting_index']:plants.sices['ending_index']]
+        self.CODES = plants.codes[plants.sices['starting_index']:plants.sices['ending_index']]
+
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=SicesPlatform.SICES_OPTIONS)
+        sices = SicesPlatform(driver)

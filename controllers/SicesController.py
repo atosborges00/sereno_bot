dccc4c34bad160f3b4e173f46b7594e3d77e0865
atosborges00 <atosborges00@gmail.com>
2021-10-01 10:48:50 -0300

    Creating the SicesController file for handling the SicesPlatform behavior

---
+++ controllers/SicesController.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from modules.SicesPlatform import SicesPlatform
+
+
+def run():
+    """ Function responsible for controlling the Sices Page during the data download """
+    pass

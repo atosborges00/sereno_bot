cc0b65f27c75b06769d215ebf04d212578854b6a
atosborges00 <atosborges00@gmail.com>
2021-10-02 15:04:46 -0300

    Adding try statements to the BasePage to handle the exception raised when the elements are not found by the checking functions of the BasePage

--- modules/BasePage.py
+++ modules/BasePage.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from os.path import isfile, join
 from os import listdir
 
@@ -25,16 +26,25 @@ class BasePage:
         return element.text
 
     def is_present(self, by_locator) -> bool:
-        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator))
-        return bool(element)
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator))
+            return bool(element)
+        except TimeoutException:
+            return False
 
     def is_visible(self, by_locator) -> bool:
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
-        return bool(element)
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+            return bool(element)
+        except TimeoutException:
+            return False
 
     def is_clickable(self, by_locator) -> bool:
-        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(by_locator))
-        return bool(element)
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(by_locator))
+            return bool(element)
+        except TimeoutException:
+            return False
 
     def check_downloads_chrome(self) -> list:
         if not self.driver.current_url.startswith("chrome://downloads"):

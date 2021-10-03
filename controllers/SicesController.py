07743e17b4b8a29eda62fa64d3c874394ecf3bd9
atosborges00 <atosborges00@gmail.com>
2021-10-03 09:28:17 -0300

    Adding the export_download_log functionality to the SicesController. Also ccreated the BaseController to use this functionality to all controllers

--- controllers/SicesController.py
+++ controllers/SicesController.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from modules.SicesPlatform import SicesPlatform
+from controllers import BaseController
 
 
-def run(plants, keys):
+def run(plants, keys, export_log=True):
+    download_log = []
     PLATFORM_INDEX = 0
     PLATFORM_PLANTS_INDICES = [index for index in range(len(plants.login_codes)) if plants.login_codes[index] == 1]
     INDIVIDUAL_PLANTS_INDICES = [index for index in range(len(plants.login_codes))
@@ -25,7 +27,15 @@ def run(plants, keys):
             checked = sices.check_download(plants.plants_names[plant])
 
             if checked:
+                download_log.append([plants.plants_names[plant], 'OK'])
                 print("{plant_name} download confirmed".format(plant_name=plants.plants_names[plant]))
 
+                if export_log:
+                    BaseController.export_log_file(download_log)
+
         if not downloaded:
+            download_log.append([plants.plants_names[plant], 'SEM DADOS'])
             print("Unable to download {plant_name} data".format(plant_name=plants.plants_names[plant]))
+
+            if export_log:
+                BaseController.export_log_file(download_log)

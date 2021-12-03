c2bb0c63c97be964b2d4eb316a306bf6221c4c36
atosborges00 <atosborges00@gmail.com>
2021-12-03 10:03:12 -0300

    Adding configuration options to ConfigAurora

--- config/ConfigAurora.py
+++ config/ConfigAurora.py
@@ -1,3 +1,4 @@
+from os import path
 from config.ConfigPaths import ConfigPaths
 
 
@@ -6,4 +7,12 @@ class ConfigAurora:
     """ Aurora login page URL """
     LOGIN_URL = "https://www.auroravision.net/ums/v1/loginPage?redirectUrl=http:%2F%2Fwww.auroravision.net%2Fdash%2Fhome.jsf&cause=MISSING_TOKEN"
 
-    """ Paths for the files containing login information """
+    """ Aurora platform default directories """
+
+    RAW_DATA_PATH = path.join(ConfigPaths.RAW_DATA_PATH, "aurora")
+
+    """ Aurora platform options settings """
+
+    PREFERENCES = {
+        'download.default_directory': RAW_DATA_PATH,
+        'safebrowsing.enabled': 'false'}

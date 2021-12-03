040bc66c2e2f6d9f195c1b83efec6ed9d1a26bcb
atosborges00 <atosborges00@gmail.com>
2021-12-03 11:02:46 -0300

    Adding the Aurora Controller to be able to test the modules while devoloping

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from config.ConfigPaths import ConfigPaths
 from util.LoginsLoader import LoginsLoader
 from util.PlantsLoader import PlantsLoader
-from controllers import SicesController
+from controllers import SicesController, AuroraController
 
 if __name__ == '__main__':
     """ Main function of the application """
 
     plants = PlantsLoader(ConfigPaths.PLANTS)
     keys = LoginsLoader(ConfigPaths.LOGINS)
+
+    AuroraController.run(plants, keys)
 
     SicesController.run(plants, keys, 'Mês Passado', folder_name='nov', sleep_time=8)
